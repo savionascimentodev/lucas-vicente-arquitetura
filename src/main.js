117bc5bb2d8a1360c624b5aa754d93239e043213
Savio Nascimento <savionascimentodev@gmail.com>
2022-07-06 20:09:56 -0400

    🔨 updated main

--- src/main.js
+++ src/main.js
@@ -1,5 +1,38 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
+import { createApp } from 'vue';
 
-createApp(App).use(router).mount('#app')
+import App from './App.vue';
+import router from './router';
+
+import 'mdb-vue-ui-kit/css/mdb.min.css';
+import 'viewerjs/dist/viewer.css'
+import VueViewer from 'v-viewer'
+import MasonryWall from '@yeger/vue-masonry-wall'
+
+
+import 'primevue/resources/themes/saga-blue/theme.css' //theme
+import 'primevue/resources/primevue.min.css' //core css
+import 'primeicons/primeicons.css' //icons
+
+import PrimeVue from 'primevue/config';
+import Dialog from 'primevue/dialog';
+import Button from 'primevue/button';
+import Carousel from 'primevue/carousel';
+import Card from 'primevue/card';
+import Divider from 'primevue/divider';
+
+const app = createApp(App);
+app.use(router);
+app.use(VueViewer)
+app.use(MasonryWall)
+app.use(PrimeVue)
+app.component('DialogComponent', Dialog);
+app.component('ButtonComponent', Button);
+app.component('CarouselComponent', Carousel);
+app.component('CardComponent', Card);
+app.component('DividerComponent', Divider);
+
+app.mount('#app');
+
+
+
+
