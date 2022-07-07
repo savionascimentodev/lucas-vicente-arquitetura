1d8314d80109ed555442227095c676d081782c92
Savio Nascimento <savionascimentodev@gmail.com>
2022-07-06 20:11:43 -0400

    ✨ added api

---
+++ src/api/projects.js
@@ -0,0 +1,299 @@
+export const projects = [
+  {
+    id: 1,
+    title: "Casa L.M.W.",
+    type: "Projeto de Interiores",
+    description: "Projeto de interiores completo de gourmet intimista e elegante;",
+    images: [
+      {
+        'id': 1,
+        src: require('@/assets/casa.L.M.V/areaGourmet01.png')
+      },
+      {
+        'id': 2,
+        src: require('@/assets/casa.L.M.V/areaGourmet02.png')
+      },
+      {
+        'id': 3,
+        src: require('@/assets/casa.L.M.V/areaGourmet03.png')
+      },
+      {
+        'id': 4,
+        src: require('@/assets/casa.L.M.V/areaGourmet04.png')
+      },
+      {
+        'id': 5,
+        src: require('@/assets/casa.L.M.V/areaGourmet05.png')
+      },
+      {
+        'id': 6,
+        src: require('@/assets/casa.L.M.V/areaGourmet06.png')
+      },
+      {
+        'id': 7,
+        src: require('@/assets/casa.L.M.V/areaGourmet07.png')
+      },
+      {
+        'id': 8,
+        src: require('@/assets/casa.L.M.V/areaGourmet08.png')
+      },
+      {
+        'id': 9,
+        src: require('@/assets/casa.L.M.V/areaGourmet09.png')
+      },
+    ]
+  },
+  {
+    id: 2,
+    title: "Ponto Vet",
+    type: "Arquitetura residencial",
+    description: "Uma casa contemporânea de alto padrão que aplica conceitos básicos de sustentabilidade",
+    images: [
+      {
+        "id": 1,
+        src: require("@/assets/pontoVet/pontoVet01.png")
+      },
+      {
+        "id": 2,
+        src: require("@/assets/pontoVet/pontoVet02.png")
+      },
+      {
+        "id": 3,
+        src: require("@/assets/pontoVet/pontoVet03.png")
+      },
+      {
+        "id": 4,
+        src: require("@/assets/pontoVet/pontoVet04.png")
+      }
+    ]
+  },
+  {
+    id: 3,
+    title: "Casa Conceito",
+    type: "Arquitetura residencial",
+    description: "Uma casa contemporânea de alto padrão que aplica conceitos básicos de sustentabilidade",
+    images: [
+      {
+        'id': 1,
+        src: require('@/assets/casaConceito/casaConceito01.png')
+      },
+      {
+        'id': 2,
+        src: require('@/assets/casaConceito/casaConceito02.png')
+      },
+      {
+        'id': 3,
+        src: require('@/assets/casaConceito/casaConceito03.png')
+      },
+      {
+        'id': 4,
+        src: require('@/assets/casaConceito/casaConceito04.png')
+      }
+    ]
+  },
+  {
+    id: 4,
+    title: "Escritório L.M.V",
+    type: "Interiores Comercial",
+    year: 2022,
+    local: 'Nova Mutum - MT',
+    area: '66,00m²',
+    status: 'Finalizado',
+    description: "Um projeto de interiores pensado para otimizar o espaço, em um ambiente onde anteriormente haviam apenas recepção e uma sala de atendimento com banheiro, o pensar correto do espaço, permitiu a criação de duas salas de atendimento, uma sala de reunião, copa, banheiro e recepção. Ambientes que atemdem a necessidade do cliente, pensando no futuro dos seus negócios, no conforto e praticidade do dia-a-dia do trabalho.",
+    englishDescription: "An interior project designed to optimize space, in an environment where previously there were only reception and a service room with bathroom, the correct thinking of the space, allowed the creation of two service rooms, a meeting room, pantry, bathroom and Front desk. Environments that meet the customer's needs, thinking about the future of their business, the comfort and practicality of day-to-day work.",
+    images: [
+      {
+        'id': 1,
+        src: require('@/assets/escritorio.L.M.V/escritorio.L.M.V01.png')
+      },
+      {
+        'id': 2,
+        src: require('@/assets/escritorio.L.M.V/escritorio.L.M.V02.png')
+
+      },
+      {
+        'id': 3,
+        src: require('@/assets/escritorio.L.M.V/escritorio.L.M.V03.png')
+
+      },
+      {
+        'id': 4,
+        src: require('@/assets/escritorio.L.M.V/escritorio.L.M.V04.png')
+
+      },
+      {
+        'id': 5,
+        src: require('@/assets/escritorio.L.M.V/escritorio.L.M.V05.png')
+
+      },
+      {
+        'id': 6,
+        src: require('@/assets/escritorio.L.M.V/escritorio.L.M.V06.png')
+
+      }
+    ]
+  }
+]
+
+export const projeto1 = {
+  "id": 1,
+  "title": "Casa L.M.W.",
+  "type": "Projeto de Interiores",
+  "description": "Projeto de interiores completo de gourmet intimista e elegante;",
+  "images": [
+    {
+      'id': 1,
+      src: require('@/assets/casa.L.M.V/areaGourmet01.png')
+    },
+    {
+      'id': 2,
+      src: require('@/assets/casa.L.M.V/areaGourmet02.png')
+    },
+    {
+      'id': 3,
+      src: require('@/assets/casa.L.M.V/areaGourmet03.png')
+    },
+    {
+      'id': 4,
+      src: require('@/assets/casa.L.M.V/areaGourmet04.png')
+    },
+    {
+      'id': 5,
+      src: require('@/assets/casa.L.M.V/areaGourmet05.png')
+    },
+    {
+      'id': 6,
+      src: require('@/assets/casa.L.M.V/areaGourmet06.png')
+    },
+    {
+      'id': 7,
+      src: require('@/assets/casa.L.M.V/areaGourmet07.png')
+    },
+    {
+      'id': 8,
+      src: require('@/assets/casa.L.M.V/areaGourmet08.png')
+    },
+    {
+      'id': 9,
+      src: require('@/assets/casa.L.M.V/areaGourmet09.png')
+    },
+  ]
+}
+
+export const projeto2 = {
+  "id": 2,
+  "title": "Ponto Vet",
+  "type": "Arquitetura residencial",
+  "description": "Uma casa contemporânea de alto padrão que aplica conceitos básicos de sustentabilidade",
+  "images": [
+    {
+      "id": 1,
+      src: require("@/assets/pontoVet/pontoVet01.png")
+    },
+    {
+      "id": 2,
+      src: require("@/assets/pontoVet/pontoVet02.png")
+    },
+    {
+      "id": 3,
+      src: require("@/assets/pontoVet/pontoVet03.png")
+    },
+    {
+      "id": 4,
+      src: require("@/assets/pontoVet/pontoVet04.png")
+    }
+  ]
+}
+
+export const projeto3 = {
+  id: 3,
+  title: "Casa Conceito",
+  type: "Arquitetura residencial",
+  description: "Uma casa contemporânea de alto padrão que aplica conceitos básicos de sustentabilidade",
+  englishDescription: "A contemporary high-end home that applies basic sustainability concepts",
+  images: [
+    {
+      'id': 1,
+      src: require('@/assets/casaConceito/casaConceito01.png')
+    },
+    {
+      'id': 2,
+      src: require('@/assets/casaConceito/casaConceito02.png')
+    },
+    {
+      'id': 3,
+      src: require('@/assets/casaConceito/casaConceito03.png')
+    },
+    {
+      'id': 4,
+      src: require('@/assets/casaConceito/casaConceito04.png')
+    }
+  ]
+}
+export const projeto4 = {
+  id: 4,
+  title: "Escritório L.M.V",
+  type: "Interiores Comercial",
+  year: 2022,
+  local: 'Nova Mutum - MT',
+  area: '66,00m²',
+  status: 'Finalizado',
+  description: "Um projeto de interiores pensado para otimizar o espaço, em um ambiente onde anteriormente haviam apenas recepção e uma sala de atendimento com banheiro, o pensar correto do espaço, permitiu a criação de duas salas de atendimento, uma sala de reunião, copa, banheiro e recepção. Ambientes que atemdem a necessidade do cliente, pensando no futuro dos seus negócios, no conforto e praticidade do dia-a-dia do trabalho.",
+  englishDescription: "An interior project designed to optimize space, in an environment where previously there were only reception and a service room with bathroom, the correct thinking of the space, allowed the creation of two service rooms, a meeting room, pantry, bathroom and Front desk. Environments that meet the customer's needs, thinking about the future of their business, the comfort and practicality of day-to-day work.",
+  images: [
+    {
+      'id': 1,
+      src: require('@/assets/escritorio.L.M.V/escritorio.L.M.V01.png')
+    },
+    {
+      'id': 2,
+      src: require('@/assets/escritorio.L.M.V/escritorio.L.M.V02.png')
+
+    },
+    {
+      'id': 3,
+      src: require('@/assets/escritorio.L.M.V/escritorio.L.M.V03.png')
+
+    },
+    {
+      'id': 4,
+      src: require('@/assets/escritorio.L.M.V/escritorio.L.M.V04.png')
+
+    },
+    {
+      'id': 5,
+      src: require('@/assets/escritorio.L.M.V/escritorio.L.M.V05.png')
+
+    },
+    {
+      'id': 6,
+      src: require('@/assets/escritorio.L.M.V/escritorio.L.M.V06.png')
+
+    },
+  ]
+}
+export const projeto5 = {
+  id: 5,
+  title: "Casa Conceito",
+  type: "Arquitetura residencial",
+  description: "Uma casa contemporânea de alto padrão que aplica conceitos básicos de sustentabilidade",
+  englishDescription: "A contemporary high-end home that applies basic sustainability concepts",
+  images: [
+    {
+      'id': 1,
+      src: require('@/assets/casaConceito/casaConceito01.png')
+    },
+    {
+      'id': 2,
+      src: require('@/assets/casaConceito/casaConceito02.png')
+    },
+    {
+      'id': 3,
+      src: require('@/assets/casaConceito/casaConceito03.png')
+    },
+    {
+      'id': 4,
+      src: require('@/assets/casaConceito/casaConceito04.png')
+    }
+  ]
+}
