c188d869a97b9133aed111e7a61dedc27976f218
Savio Nascimento <savionascimentodev@gmail.com>
2022-07-06 20:09:40 -0400

    ✨ added new routes, about and gallery

--- src/router/index.js
+++ src/router/index.js
@@ -8,18 +8,21 @@ const routes = [
     component: HomeView
   },
   {
-    path: '/about',
+    path: '/about/:id',
     name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+    component: () => import('../views/AboutView.vue'),
+    props: true
+  },
+  {
+    path: '/gallery',
+    name: 'gallery',
+    component: () => import('../views/GalleryView.vue'),
   }
 ]
 
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
-  routes
+  routes,
 })
 
 export default router
