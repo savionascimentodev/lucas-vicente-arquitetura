5693ccf13b9c56c15d2ffdc83b1fb7811283fdf1
Savio Nascimento <savionascimentodev@gmail.com>
2022-08-04 19:14:47 -0400

    ✨ added gallery api

---
+++ src/api/gallery.js
@@ -0,0 +1,287 @@
+export const project1 = {
+  id: 1,
+  title: "Casa L.M.W.",
+  type: "Projeto de Interiores",
+  "images": [
+    {
+      id: 1,
+      src: require('@/assets/casa.L.M.V/areaGourmet01.png')
+    },
+    {
+      id: 2,
+      src: require('@/assets/casa.L.M.V/areaGourmet02.png')
+    },
+    {
+      id: 3,
+      src: require('@/assets/casa.L.M.V/areaGourmet03.png')
+    },
+    {
+      id: 4,
+      src: require('@/assets/casa.L.M.V/areaGourmet04.png')
+    },
+    {
+      id: 5,
+      src: require('@/assets/casa.L.M.V/areaGourmet05.png')
+    },
+    {
+      id: 6,
+      src: require('@/assets/casa.L.M.V/areaGourmet06.png')
+    },
+    {
+      id: 7,
+      src: require('@/assets/casa.L.M.V/areaGourmet07.png')
+    },
+    {
+      id: 8,
+      src: require('@/assets/casa.L.M.V/areaGourmet08.png')
+    },
+    {
+      id: 9,
+      src: require('@/assets/casa.L.M.V/areaGourmet09.png')
+    },
+  ]
+}
+export const project2 = {
+  id: 2,
+  title: "Internação Veterinária",
+  type: "Reforma",
+  images: [
+    {
+      id: 1,
+      src: require("@/assets/pontoVet/pontoVet01.png")
+    },
+    {
+      id: 2,
+      src: require("@/assets/pontoVet/pontoVet02.png")
+    },
+    {
+      id: 3,
+      src: require("@/assets/pontoVet/pontoVet03.png")
+    },
+    {
+      id: 4,
+      src: require("@/assets/pontoVet/pontoVet04.png")
+    },
+    {
+      id: 5,
+      src: require("@/assets/pontoVet/pontoVet05.png")
+    }
+  ]
+}
+export const project4 = {
+  id: 4,
+  title: "Escritório L.M.V",
+  type: "Interiores Comercial",
+  images: [
+    {
+      'id': 1,
+      src: require('@/assets/escritorio.L.M.V/escritorio.L.M.V01.png')
+    },
+    {
+      'id': 2,
+      src: require('@/assets/escritorio.L.M.V/escritorio.L.M.V02.png')
+
+    },
+    {
+      'id': 3,
+      src: require('@/assets/escritorio.L.M.V/escritorio.L.M.V03.png')
+
+    },
+    {
+      'id': 4,
+      src: require('@/assets/escritorio.L.M.V/escritorio.L.M.V04.png')
+
+    },
+    {
+      'id': 5,
+      src: require('@/assets/escritorio.L.M.V/escritorio.L.M.V05.png')
+
+    },
+    {
+      'id': 6,
+      src: require('@/assets/escritorio.L.M.V/escritorio.L.M.V06.png')
+    },
+    {
+      'id': 7,
+      src: require('@/assets/escritorio.L.M.V/escritorio.L.M.V07.png')
+    },
+    {
+      'id': 8,
+      src: require('@/assets/escritorio.L.M.V/escritorio.L.M.V08.png')
+    },
+    {
+      'id': 9,
+      src: require('@/assets/escritorio.L.M.V/escritorio.L.M.V09.png')
+    },
+    {
+      'id': 10,
+      src: require('@/assets/escritorio.L.M.V/escritorio.L.M.V10.png')
+    },
+    {
+      'id': 11,
+      src: require('@/assets/escritorio.L.M.V/escritorio.L.M.V11.png')
+    },
+    {
+      'id': 12,
+      src: require('@/assets/escritorio.L.M.V/escritorio.L.M.V12.png')
+    },
+    {
+      'id': 13,
+      src: require('@/assets/escritorio.L.M.V/escritorio.L.M.V13.png')
+    },
+    {
+      'id': 14,
+      src: require('@/assets/escritorio.L.M.V/escritorio.L.M.V14.png')
+    },
+    {
+      'id': 15,
+      src: require('@/assets/escritorio.L.M.V/escritorio.L.M.V15.png')
+    },
+    {
+      'id': 16,
+      src: require('@/assets/escritorio.L.M.V/escritorio.L.M.V16.png')
+    },
+    {
+      'id': 17,
+      src: require('@/assets/escritorio.L.M.V/escritorio.L.M.V17.png')
+    },
+    {
+      'id': 18,
+      src: require('@/assets/escritorio.L.M.V/escritorio.L.M.V18.png')
+    },
+    {
+      'id': 19,
+      src: require('@/assets/escritorio.L.M.V/escritorio.L.M.V19.png')
+    },
+    {
+      'id': 20,
+      src: require('@/assets/escritorio.L.M.V/escritorio.L.M.V20.png')
+    },
+    {
+      'id': 21,
+      src: require('@/assets/escritorio.L.M.V/escritorio.L.M.V21.png')
+    },
+    {
+      'id': 22,
+      src: require('@/assets/escritorio.L.M.V/escritorio.L.M.V22.png')
+    },
+    {
+      'id': 23,
+      src: require('@/assets/escritorio.L.M.V/escritorio.L.M.V23.png')
+    },
+  ]
+}
+
+
+// Dados Errados
+export const project5 = {
+  id: 5,
+  title: "Escritório L.M.V",
+  type: "Interiores Comercial",
+  images: [
+    {
+      id: 1,
+      src: require('@/assets/casaMC/casaMC01.png')
+    },
+    {
+      id: 2,
+      src: require('@/assets/casaMC/casaMC02.png')
+    },
+    {
+      id: 3,
+      src: require('@/assets/casaMC/casaMC03.png')
+
+    },
+    {
+      id: 4,
+      src: require('@/assets/casaMC/casaMC04.png')
+
+    },
+    {
+      id: 5,
+      src: require('@/assets/casaMC/casaMC05.png')
+
+    },
+    {
+      id: 6,
+      src: require('@/assets/casaMC/casaMC06.png')
+
+    },
+    {
+      id: 7,
+      src: require('@/assets/casaMC/casaMC07.png')
+
+    }
+  ]
+}
+
+export const project7 = {
+  id: 7,
+  title: "Casa L.V.S",
+  type: "Interiores Residencial",
+  images: [
+    {
+      id: 1,
+      src: require('@/assets/casaLVS/casaLVS01.png')
+    },
+    {
+      id: 2,
+      src: require('@/assets/casaLVS/casaLVS02.png')
+    },
+    {
+      id: 3,
+      src: require('@/assets/casaLVS/casaLVS03.png')
+    },
+    {
+      id: 4,
+      src: require('@/assets/casaLVS/casaLVS04.png')
+    },
+    {
+      id: 5,
+      src: require('@/assets/casaLVS/casaLVS05.png')
+    },
+    {
+      id: 6,
+      src: require('@/assets/casaLVS/casaLVS06.png')
+    },
+    {
+      id: 7,
+      src: require('@/assets/casaLVS/casaLVS07.png')
+    },
+    {
+      id: 8,
+      src: require('@/assets/casaLVS/casaLVS08.png')
+    },
+    {
+      id: 9,
+      src: require('@/assets/casaLVS/casaLVS09.png')
+    },
+  ]
+}
+export const project8 = {
+  id: 8,
+  title: "Investindo na constução",
+  type: "Arquitetura de Uso Misto(Investimento)",
+  images: [
+    {
+      id: 1,
+      src: require('@/assets/investindoConstrução/investindoConstrução01.png')
+    },
+    {
+      id: 2,
+      src: require('@/assets/investindoConstrução/investindoConstrução02.png')
+    },
+    {
+      id: 3,
+      src: require('@/assets/investindoConstrução/investindoConstrução03.png')
+    },
+    {
+      id: 4,
+      src: require('@/assets/investindoConstrução/investindoConstrução05.png')
+    },
+    {
+      id: 5,
+      src: require('@/assets/investindoConstrução/investindoConstrução06.png')
+    },
+  ]
+}
