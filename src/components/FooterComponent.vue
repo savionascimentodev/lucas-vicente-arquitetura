f69c72c16df49aae9ba15d11852488458ce9a9f5
Savio Nascimento <savionascimentodev@gmail.com>
2022-09-20 17:55:33 -0400

    ♻️ refactored footer

--- src/components/FooterComponent.vue
+++ src/components/FooterComponent.vue
@@ -1,49 +1,52 @@
 <template>
-  <MDBFooter
-    style="
-      background-image: linear-gradient(to right, #434343 0%, black 100%);
-      color: white;
-    "
-  >
-    <MDBContainer class="text-center text-md-start pt-4">
-      <MDBRow>
-        <MDBCol md="4" lg="3" xl="3" class="mx-auto mb-md-0 mb-4">
-          <img
-            src="@/assets/lucas_vicente-logo_black-200x515.png"
-            class="hover-shadow img-fluid rounded"
-            alt="logo Lucas Vicente"
-            width="450"
-          />
-        </MDBCol>
+  <MDBFooter>
+    <div class="footer-container">
+      <MDBContainer class="text-center text-md-start pt-4">
+        <MDBRow>
+          <MDBCol md="4" class="footer-description mx-auto mb-md-0 mb-4 pb-3">
+            <img
+              src="@/assets/logoClaro.png"
+              class="logo hover-shadow img-fluid rounded"
+              alt="logo Lucas Vicente"
+            />
+          </MDBCol>
 
-        <MDBCol md="4" lg="3" xl="3" class="mx-auto mb-md-0 mb-4 text-white">
-          <h6 class="text-uppercase fw-bold mb-2">Contato</h6>
-          <p>
-            <MDBIcon icon="envelope" class="me-3" />lucasvicentenn@gmail.com
-          </p>
-          <p><MDBIcon icon="phone" class="me-3" />(65) 9933-1027</p>
-        </MDBCol>
-
-        <MDBCol md="3" lg="2" xl="2" class="mx-auto mb-4 text-white">
-          <h6 class="text-uppercase fw-bold mb-4">Redes Socias</h6>
-          <a
-            href="https://www.instagram.com/lucasvicentenn"
-            target="_blank"
-            class="me-4 text-reset"
+          <MDBCol
+            md="4"
+            class="footer-description mx-auto mb-md-0 mb-4 text-white"
           >
-            <MDBIcon iconStyle="fab" icon="instagram"></MDBIcon>
-          </a>
-        </MDBCol>
-      </MDBRow>
-    </MDBContainer>
-    <div
-      class="text-center p-3 text-white"
-      style="background-color: rgba(0, 0, 0, 0.05)"
-    >
-      © 2021 Copyright:
-      <a class="text-reset fw-bold" href="https://mdbootstrap.com/">
-        Lucas Vicente Arquitetura
-      </a>
+            <h6 class="footer-title text-uppercase fw-bold mb-2">Contato</h6>
+            <p>
+              <MDBIcon icon="envelope" class="me-3" />lucasvicentenn@gmail.com
+            </p>
+            <p><MDBIcon icon="phone" class="me-3" />(65) 9933-1027</p>
+            <p>
+              <a
+                href="https://www.instagram.com/lucasvicentenn"
+                target="_blank"
+                class="text-reset"
+              >
+                <MDBIcon iconStyle="fab" class="me-3" icon="instagram" />
+                lucasvicentenn
+              </a>
+            </p>
+          </MDBCol>
+
+          <MDBCol md="3" class="footer-description mx-auto text-white">
+            <h6 class="footer-title text-uppercase fw-bold mb-0">
+              Conselho de arquitetura
+            </h6>
+            <h6 class="footer-title text-uppercase fw-bold mb-4">
+              e urbanismo
+            </h6>
+            <p><MDBIcon icon="building" class="me-3" />A271919-3</p>
+          </MDBCol>
+        </MDBRow>
+      </MDBContainer>
+      <div class="text-center p-3 text-white">
+        © 2021 Copyright:
+        <a class="text-reset fw-bold"> Lucas Vicente Arquitetura </a>
+      </div>
     </div>
   </MDBFooter>
 </template>
@@ -68,5 +71,45 @@ export default {
   }
 }
 </script>
+<style lang="scss">
+.footer-container {
+  background: #333333ff;
+}
+</style>
+<style lang="scss" scoped>
+// Medias ---
 
-<style scoped lang="scss"></style>
+@media screen and (min-width: 320px) {
+  .logo {
+    width: 60%;
+  }
+  .footer-title {
+    font-size: 1rem;
+  }
+
+  .footer-description {
+    border-bottom: 1px solid #fff;
+  }
+}
+
+@media screen and (min-width: 480px) {
+  .logo {
+    width: 65%;
+  }
+  .footer-title {
+    font-size: 1.3rem;
+  }
+}
+
+@media screen and (min-width: 769px) {
+  .logo {
+    width: 90%;
+  }
+}
+
+@media screen and (min-width: 1025px) {
+  .logo {
+    width: 90%;
+  }
+}
+</style>
