5c5c67df0ffdba0ffc4ee5ec0d73fa970e959854
Savio Nascimento <savionascimentodev@gmail.com>
2023-01-18 17:08:49 -0400

    ➕ added vueI18n

---
+++ src/locales/pt_BR.js
@@ -0,0 +1,3 @@
+export const pt_BR = {
+  hello: 'oii',
+}
--- src/main.js
+++ src/main.js
@@ -1,12 +1,28 @@
 import { createApp } from 'vue';
-
 import App from './App.vue';
 import router from './router';
 
+// Translate
+import { createI18n } from 'vue-i18n'
+import { pt_BR } from '@/locales/pt_BR'
+import { en } from '@/locales/en'
+
+const languages = {
+  pt_BR,
+  en
+}
+
+const i18n = createI18n({
+  locale: 'pt_BR',
+  fallbackLocale: 'pt_BR',
+  messages: languages
+})
+
+
+// V-viewer e MdbVue
 import 'mdb-vue-ui-kit/css/mdb.min.css';
 import 'viewerjs/dist/viewer.css'
 import VueViewer from 'v-viewer'
-import MasonryWall from '@yeger/vue-masonry-wall'
 
 
 import 'primevue/resources/themes/saga-blue/theme.css' //theme
@@ -20,13 +36,15 @@ import Button from 'primevue/button';
 import Carousel from 'primevue/carousel';
 import Card from 'primevue/card';
 import Divider from 'primevue/divider';
+import CascadeSelect from 'primevue/cascadeselect';
 
 const app = createApp(App);
 app.use(router);
 app.use(VueViewer)
-app.use(MasonryWall)
 app.use(PrimeVue)
+app.use(i18n)
 app.component('DialogComponent', Dialog);
+app.component('CascadeSelect ', CascadeSelect)
 app.component('ScrollTop', ScrollTop);
 app.component('ButtonComponent', Button);
 app.component('CarouselComponent', Carousel);
