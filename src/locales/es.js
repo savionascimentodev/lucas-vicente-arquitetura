550bba7278bf0705931978e3b6630448df867015
Savio Nascimento <savionascimentodev@gmail.com>
2023-01-21 18:13:23 -0400

    🌐 added language spain

---
+++ src/locales/es.js
@@ -0,0 +1,17 @@
+export const es = {
+  nav: {
+    home: 'Home',
+    gallery: 'Galería',
+    contact: 'Contacto',
+    architecture: 'Arquitectura',
+    language: 'Idioma',
+    portuguese: 'Portugués',
+    english: 'Inglés',
+    spanish: 'Español'
+  },
+  footer: {
+    contact: 'contacto',
+    architecturalCouncil: 'asesoramiento arquitectonico',
+    urbanism: 'y urbanismo'
+  }
+}
