926f87ce456acf9e479abcd841f7f24ece54c8f5
Savio Nascimento <savionascimentodev@gmail.com>
2023-05-01 20:17:22 -0400

    ✨ tentativa de colocar cache

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,24 @@
 const { defineConfig } = require('@vue/cli-service')
+const CompressionPlugin = require('compression-webpack-plugin')
+
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  filenameHashing: true,
+  chainWebpack: config => {
+    config.output.filename('[name].[hash].js').end()
+  },
+  configureWebpack: config => {
+    if (process.env.NODE_ENV === 'production') {
+      return {
+        plugins: [
+          new CompressionPlugin({
+            test: /\.(js|css|html|svg)$/,
+            threshold: 10240,
+            minRatio: 0.8,
+            deleteOriginalAssets: false
+          })
+        ]
+      }
+    }
+  }
 })
