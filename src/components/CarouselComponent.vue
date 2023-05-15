2e5b666a220bca751f8fa219c49d0d2b2c098328
savionascimento <savionascimentodev@gmail.com>
2023-05-15 11:10:23 -0400

    ✨ add removed components??

---
+++ src/components/CarouselComponent.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="carousel-container">
+    <MDBCarousel :items="project.images" :controls="true" fade />
+  </div>
+</template>
+
+<script>
+import { MDBCarousel } from 'mdb-vue-ui-kit'
+import { project3 } from '@/api/projects.js'
+
+export default {
+  name: 'CarouselComponent',
+  components: {
+    MDBCarousel
+  },
+  data() {
+    return {
+      project: project3
+    }
+  }
+}
+</script>
